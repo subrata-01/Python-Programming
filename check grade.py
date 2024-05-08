eb75eae90d0cf6b12e5c3c7d63a0d79ef40a5bd3
Subrata Mahato <168007419+subrata-01@users.noreply.github.com>
2024-05-08 16:13:54 +0530

    Add files via upload

---
+++ check grade.py
@@ -0,0 +1,13 @@
+marks=int(input("enter your marks in hundred : "))
+if marks>90:
+    print("grade-A")
+elif marks>80:
+    print("grade-B")
+elif marks>70:
+    print("grade-C")
+elif marks>60:
+    print("grade-D")
+elif marks>=50:
+    print("grade-E")
+else:
+    print("grade-F")
