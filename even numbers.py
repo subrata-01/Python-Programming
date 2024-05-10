010d9711740280e125d2f6a1127d3f8547a9ae3e
Subrata Mahato <168007419+subrata-01@users.noreply.github.com>
2024-05-10 09:33:17 +0530

    Add files via upload

---
+++ even numbers.py
@@ -0,0 +1,6 @@
+n=int(input("enter range of the series : "))
+print("The even number series is : ")
+i=1
+while i<=n:
+    print(2*i,end=" ")
+    i+=1
---
+++ factorial of a num.py
@@ -0,0 +1,6 @@
+n=int(input("enter a number : "))
+fact=1
+while n>0:
+    fact=fact*n
+    n-=1
+print("The factorial is : ",fact)
